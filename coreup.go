4594615b85ebd6eee0caeadbc9ddc5db38f1c524
Blake Mizerany <blake.mizerany@gmail.com>
2014-03-30 23:11:48 -0700

    clean up flag parsing

--- coreup.go
+++ coreup.go
@@ -22,47 +22,49 @@ func getClient(project string, region string) (CoreClient, error) {
 	}
 	return nil, errors.New("Unable to find provider")
 }
+
+var usr *user.User
+
+func init() {
+	usr, _ = user.Current()
+}
+
+var (
+	cloudConfig = flag.String("cloud-config", "", "local file, usually ./cloud-config.yml")
+	channel     = flag.String("channel", "alpha", "CoreOS channel to use")
+	provider    = flag.String("provider", "ec2", "cloud or provider to launch instance in")
+	region      = flag.String("region", "us-west-2", "region to launch instance in")
+	action      = flag.String("action", "run", "run, terminate, list")
+	size        = flag.String("size", "m1.medium", "size of instance")
+	project     = flag.String("project", "coreup-"+usr.Username, "name for the group of servers in the same project")
+	num         = flag.Int("num", 1, "number of instances to launch like this")
+)
+
 func main() {
-	var cloudConfigFlag string
-	flag.StringVar(&cloudConfigFlag, "cloud-config", "", "local file, usually ./cloud-config.yml")
-	var channelFlag string
-	flag.StringVar(&channelFlag, "channel", "alpha", "CoreOS channel to use")
-	var providerFlag string
-	flag.StringVar(&providerFlag, "provider", "ec2", "cloud or provider to launch instance in")
-	var regionFlag string
-	flag.StringVar(&regionFlag, "region", "us-west-2", "region to launch instance in")
-	var actionFlag string
-	flag.StringVar(&actionFlag, "action", "run", "run, terminate, list")
-	var sizeFlag string
-	flag.StringVar(&sizeFlag, "size", "m1.medium", "size of instance")
-	var projectFlag string
-	user, _ := user.Current()
-	flag.StringVar(&projectFlag, "project", "coreup-"+user.Username, "name for the group of servers in the same project")
-	var numFlag int
-	flag.IntVar(&numFlag, "num", 1, "number of instances to launch like this")
 	flag.Parse()
+
 	var cloud_config string
-	if cloudConfigFlag != "" {
-		data, err := ioutil.ReadFile(cloudConfigFlag)
+	if *cloudConfig != "" {
+		data, err := ioutil.ReadFile(*cloudConfig)
 		if err != nil {
 			println("unable to read cloud-config")
 		}
 		cloud_config = string(data)
 	}
-	c, err := getClient(providerFlag, regionFlag)
+	c, err := getClient(*provider, *region)
 	if err != nil {
-		fmt.Println("error getting client", providerFlag, err)
+		fmt.Println("error getting client", *provider, err)
 		os.Exit(1)
 	}
-	switch actionFlag {
+	switch *action {
 	case "run":
-		err = c.Run(projectFlag, channelFlag, regionFlag, sizeFlag, numFlag, cloud_config)
+		err = c.Run(*project, *channel, *region, *size, *num, cloud_config)
 		if err != nil {
 			fmt.Println("error launching instances", err)
 			os.Exit(1)
 		}
 	case "terminate":
-		err = c.Terminate(projectFlag)
+		err = c.Terminate(*project)
 		if err != nil {
 			fmt.Println("error terminating instances", err)
 			os.Exit(1)
